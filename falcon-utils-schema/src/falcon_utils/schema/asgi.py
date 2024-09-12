4d2b6870049bb6723662e9fca0c56a186351fbde
Nirbhay Gupta <nirbhay.gupta@junio.in>
2024-09-12 19:40:03 +0530

    [fix] schema module

--- falcon-utils-schema/src/falcon_utils/schema/asgi.py
+++ falcon-utils-schema/src/falcon_utils/schema/asgi.py
@@ -9,18 +9,18 @@ def map_data(schema: Union[Schema]) -> Callable:
         media = await req.media
         if hasattr(schema, 'Schema'):
             # provided schema is a marshmallow dataclass
-            req.context.data = schema.Schema().load(**media)
+            req.context.data = schema.Schema().load(media)
         else:
             req.context.data = schema(**media)
     return hook
 
 def map_query(schema: Union[Schema]) -> Callable:
     async def hook(req, resp, resource, params):
-        media = await req.params
+        media = req.params
         
         if hasattr(schema, 'Schema'):
             # provided schema is a marshmallow dataclass
-            req.context.query = schema.Schema().load(**media)
+            req.context.query = schema.Schema().load(media)
         else:
             req.context.query = schema(**media)
     return hook
